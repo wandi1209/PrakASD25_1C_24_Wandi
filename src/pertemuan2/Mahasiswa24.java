b2a22b0225b26bdc91e5bf68fa3d3bec32dd4cef
wandi1209 <wandider@gmail.com>
2025-02-18 09:55:33 +0700

    P2_Perobaan3

--- src/pertemuan2/Mahasiswa24.java
+++ src/pertemuan2/Mahasiswa24.java
@@ -6,6 +6,16 @@ public class Mahasiswa24 {
     String kelas;
     double ipk;
 
+    public Mahasiswa24(){
+        
+    }
+    public Mahasiswa24(String nm, String nim, double ipk, String kls){
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi(){
         System.out.println("Nama : " + nama);
         System.out.println("NIM : " + nim);
--- src/pertemuan2/MahasiswaMain.java
+++ src/pertemuan2/MahasiswaMain.java
@@ -7,10 +7,14 @@ public class MahasiswaMain {
         mhs1.nim = "244107020003";
         mhs1.kelas = "TI-1C";
         mhs1.ipk = 3.55;
-
+        
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("TI-2C");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa24 mhs2 = new Mahasiswa24("Annisa Nabila", "2141720160", 3.25, "TI-2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
