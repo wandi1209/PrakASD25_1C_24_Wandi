94927a4cbf0ca348f59c17102543d64283bd1350
wandi1209 <wandider@gmail.com>
2025-02-25 10:40:20 +0700

    P3_Percobaan3_PertanyaanNo4

--- src/pertemuan3/MataKuliahDemo24.java
+++ src/pertemuan3/MataKuliahDemo24.java
@@ -1,8 +1,14 @@
 package pertemuan3;
 
+import java.util.Scanner;
+
 public class MataKuliahDemo24 {
     public static void main(String[] args) {
-        MataKuliah24[] arrayOfMataKuliah = new MataKuliah24[3];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah mata kuliah : ");
+        int jumlahMataKuliah = sc.nextInt();
+        sc.nextLine();
+        MataKuliah24[] arrayOfMataKuliah = new MataKuliah24[jumlahMataKuliah];
 
         for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Masukkan Data Mata Kuliah ke-" + (i+1));
