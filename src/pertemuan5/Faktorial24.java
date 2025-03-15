5d5c1ba60985a8942621d3c7c85a242d3266659d
wandi1209 <wandider@gmail.com>
2025-03-15 12:35:34 +0700

    P5_Percobaan1_PertanyaanNo2

--- src/pertemuan5/Faktorial24.java
+++ src/pertemuan5/Faktorial24.java
@@ -3,8 +3,10 @@ package pertemuan5;
 public class Faktorial24 {
     int faktorialBF(int n){
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto *= i;
+            i++;
         }
         return fakto;
     }
