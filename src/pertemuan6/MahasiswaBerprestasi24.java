4098f8ba46115ff6222ec43ff4f4b349d410caa1
wandi1209 <wandider@gmail.com>
2025-03-24 14:24:00 +0700

    P6_Percobaan4

--- src/pertemuan6/MahasiswaBerprestasi24.java
+++ src/pertemuan6/MahasiswaBerprestasi24.java
@@ -45,4 +45,16 @@ public class MahasiswaBerprestasi24 {
             listMhs[i] = tmp;
         }
     }
+
+    void insertionSort(){
+        for (int i = 0; i < listMhs.length; i++) {
+            Mahasiswa24 temp = listMhs[i];
+            int  j=1;
+            while (j>0 && listMhs[j-1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j-1];
+                j--;
+            }   
+            listMhs[j] = temp;
+        }
+    }
 }
--- src/pertemuan6/MahasiswaDemo24.java
+++ src/pertemuan6/MahasiswaDemo24.java
@@ -33,5 +33,9 @@ public class MahasiswaDemo24 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
         list.selectionSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
     }
 }
